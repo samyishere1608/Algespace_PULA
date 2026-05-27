1b192ff5e0443a59ef3038fcc33dfec1e9a9a012
Samarth Joshi <samarthjoshi1608@gmail.com>
2026-05-27 14:58:50 +0200

    Fix DBUtils.cs: switch to Microsoft.Data.Sqlite

--- webapi/Models/Database/DBUtils.cs
+++ webapi/Models/Database/DBUtils.cs
@@ -1,11 +1,11 @@
 ﻿using Dapper;
-using System.Data.SQLite;
+using Microsoft.Data.Sqlite;
 
 namespace webapi.Models.Database
 {
     public static class DBUtils
     {
-        public static void CreateOrClearTable(SQLiteConnection connection, string tableName, string tableScheme, bool clearTable = true)
+        public static void CreateOrClearTable(SqliteConnection connection, string tableName, string tableScheme, bool clearTable = true)
         {
             string checkTableExistsQuery = "SELECT name FROM sqlite_master WHERE type='table' AND name=@TableName;";
             var result = connection.Query<string>(checkTableExistsQuery, new { TableName = tableName });
@@ -36,7 +36,7 @@ namespace webapi.Models.Database
         public static List<ExerciseResponse> GetExerciseIdsFromTable(string dbName, string tableName)
         {
             List<ExerciseResponse> ids;
-            using (var connection = new SQLiteConnection("Data Source=" + DBSettings.GetDBLocation(dbName)))
+            using (var connection = new SqliteConnection("Data Source=" + DBSettings.GetDBLocation(dbName)))
             {
                 connection.Open();
                 string selectQuery = $"SELECT Id, Level FROM {tableName} ORDER BY Ordering ASC NULLS LAST";
